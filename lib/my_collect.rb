9365c35695ac59b1f54a3568ffdf130598af70d6
Burbage Stacey <rstacey06@gmail.com>
2019-11-07 18:25:11 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,18 +1,9 @@
-def my_each (array)
-    i = 0
-    while i < array.length
-    yield(array[i])
-    i = i + 1
-    end
-    array
-  end
-  
-  def my_collect(array)
+def my_collect(array)
   i = 0
   collect = []
   while i < array.length
     collect << yield(array[i])
-    i+=1
+    i += 1
   end
   collect
 end
